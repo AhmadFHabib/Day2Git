d123b3453f2866e6401bd3e8673c80dbdc8ff1ac
Ahmad Farhad Habib <Ahmad Farhad Habib@10.0.0.21>
2023-04-07 21:00:24 -0400

    updated getstr ()

--- src/Da2/Day2Demo.java
+++ src/Da2/Day2Demo.java
@@ -3,8 +3,14 @@ package Da2;
 public class Day2Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Day 2 Demo");
 	}
+	
+	public void  getstr () {
+		System.out.println("Getstr");
+		
+		System.out.println("Puch to GitHub");
+	}
+	
 
 }
